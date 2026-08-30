49339e513730f018c2ed7b39bf102f08cb36ba40
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2026-08-30 18:49:04 +0300

    Initial import

---
+++ validate.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# PYTHON_ARGCOMPLETE_OK
+from abc import ABC, abstractmethod
+
+
+class Validator(ABC):
+    def __init__(self, **kwargs):
+        for k, v in kwargs.items():
+            setattr(self, k, v)
+
+    def __set_name__(self, owner, name):
+        self.sys_name = f"sys_{name}"
+        self.usr_name = f"usr_{name}"
+
+    def __get__(self, instance, owner):
+        if instance is None:
+            return self
+        return getattr(instance, self.sys_name)
+
+    @abstractmethod
+    def validate(self, value):
+        pass
+
+    def __set__(self, instance, value):
+        self.validate(value)
+        setattr(instance, self.sys_name, value)
+
+
+class Typed(Validator):
+    def validate(self, value):
+        if not isinstance(value, self.expected_type):
+            raise TypeError(f"{value!r}: expected {self.expected_type!r}")
+
+
+class Integer(Typed):
+    expected_type = int
+
+
+class Float(Typed):
+    expected_type = float
+
+
+class MaxSized(Validator):
+    def __init__(self, **kwargs):
+        if "size" not in kwargs:
+            raise TypeError("Must set size")
+        super().__init__(**kwargs)
+
+
+class String(Typed):
+    expected_type = str
+
+
+class SizedString(String, MaxSized):
+    def validate(self, value):
+        if len(value) < 1 or len(value) > self.size:
+            raise ValueError(f"{value} must be in lenght {self.size} or less")
+
+
+class Member(Validator):
+    def __init__(self, **kwargs):
+        _kwargs = dict(kwargs)
+        if 'size' in _kwargs:
+            del _kwargs['size']
+        super().__init__(_kwargs)
+        
+        self.members = list[_kwargs]
+    def validate(self, value):
+        if value not in self.members:
+            raise ValueError(f"{value!} must be one of {self.members}")
+
+class Unsigned(Validator):
+    def validate(self, value):
+        if value < 0:
+            raise ValueError(f"{value}: must be >0")
+
+
+class UnsignedInteger(Integer, Unsigned):
+    pass
+
+
+class UnsignedFloat(Float, Unsigned):
+    pass
+
+
+class Component:
+    name = SizedString(8)
+    price = UnsignedFloat()
+    shares = UnsignedInteger()
+    color = Member("RED", "GREEN", "BLUE")
+
+
+if __name__ == "__main__":
+    component = Component()
