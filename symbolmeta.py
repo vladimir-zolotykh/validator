4565416c2c766c0862ebd59a9c9269d1e786c947
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2026-09-03 17:52:17 +0300

    Add test_symbol()

--- symbolmeta.py
+++ symbolmeta.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # PYTHON_ARGCOMPLETE_OK
+import re
 
 
 class SymbolMeta(type):
     _symbols = {}
 
-    def __call__(cls, name, pat):
+    def __call__(cls, name, pat=""):
         if name not in SymbolMeta._symbols:
             SymbolMeta._symbols[name] = super().__call__(name, pat)
         return SymbolMeta._symbols[name]
@@ -19,11 +20,21 @@ class Symbol(metaclass=SymbolMeta):
         self.pat = pat
 
     def __repr__(self):
-        return f"Symbol({self.name!r}, {self.pat!r})"
+        return f"Symbol({self.name}, {self.pat})"
+
+
+def test_symbol():
+    SymbolMeta._symbols.clear()
+    name = Symbol("NAME", r"[A-Za-z_]\w*")
+    num = Symbol("NUM", r"\d+")
+    assert str(name) == "Symbol(NAME, [A-Za-z_]\\w*)"
+    assert str(num) == "Symbol(NUM, \\d+)"
+    num2 = Symbol("NUM")
+    assert num is num2
 
 
 if __name__ == "__main__":
-    # name = Symbol("NAME", r"[A-Za-z_]\w*")
+    name = Symbol("NAME", r"[A-Za-z_]\w*")
     num = Symbol("NUM", r"\d+")
     print(num)
     num2 = Symbol("NUM", r"\d+")
