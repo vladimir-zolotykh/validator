3ecce57be73847a4cfbcbbc69b371f15a09e4bab
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2026-09-03 23:35:59 +0300

    Refactor with `symbols' fixture

--- symbolmeta.py
+++ symbolmeta.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # PYTHON_ARGCOMPLETE_OK
+import pytest
 
 
 class SymbolMeta(type):
@@ -35,24 +36,27 @@ class Symbol(metaclass=SymbolMeta):
         return f"Symbol({self.name}, {self.pat})"
 
 
-def test_symbol_eq():
+@pytest.fixture
+def symbols():
     SymbolMeta._symbols.clear()
     name = Symbol("NAME", r"[A-Za-z_]\w*")
+    num = Symbol("NUM", r"\d+")
+    return name, num
+
+
+def test_symbol_eq(symbols):
+    name, num = symbols
     assert name == "NAME"
     assert name == Symbol("NAME")
 
 
-def test_masterpat():
-    SymbolMeta._symbols.clear()
-    Symbol("NAME", r"[A-Za-z_]\w*")
-    Symbol("NUM", r"\d+")
+def test_masterpat(symbols):
+    name, num = symbols
     assert SymbolMeta.masterpat() == "(?P<NAME>[A-Za-z_]\\w*)|(?P<NUM>\\d+)"
 
 
-def test_symbol():
-    SymbolMeta._symbols.clear()
-    name = Symbol("NAME", r"[A-Za-z_]\w*")
-    num = Symbol("NUM", r"\d+")
+def test_symbol(symbols):
+    name, num = symbols
     assert str(name) == "Symbol(NAME, [A-Za-z_]\\w*)"
     assert str(num) == "Symbol(NUM, \\d+)"
     num2 = Symbol("NUM")
